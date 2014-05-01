dd08c1ece2ac52e70b998e2f5ca2def4df1ebacb
ruiteru <fanta.good0052@gmail.com>
2014-05-01 12:18:49 +0900

    あ。

--- js/slidein.js
+++ js/slidein.js
@@ -1,7 +1,16 @@
-var slideinFoxkeh = function(){
+var endSlideinFoxkeh = function(){
+	var foxkeh = document.querySelector("#slidein-foxkeh img");
+
+	foxkeh.setAttribute("class", "");
+};
+
+var startSlideinFoxkeh = function(){
 	var foxkeh = document.querySelector("#slidein-foxkeh img");
 	foxkeh.setAttribute("class", "slidein");
+
+	foxkeh.addEventListener("animationend", endSlideinFoxkeh);
 };
 
 var slideinFoxkehButton = document.querySelector("#slidein-foxkeh button");
-slideinFoxkehButton.addEventListener("mouseover", slideinFoxkeh);
+slideinFoxkehButton.addEventListener("click", startSlideinFoxkeh);
+
